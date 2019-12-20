49b8814c202623558c95b570faf4a20e88b431ba
Benjamin Schmithüsen <schmithuesen@ux3d.io>
2019-12-20 15:15:26 +0100

    remove KTX version and compression quality from help text

--- cli/source/cli.cpp
+++ cli/source/cli.cpp
@@ -30,8 +30,6 @@ int main(int argc, char* argv[])
 		printf("-sampleCount: number of samples used for filtering (default = 1024)\n");
 		printf("-mipLevelCount: number of mip levels of specular cube map\n");
 		printf("-cubeMapResolution: resolution of output cube map (default = 1024)\n");
-		printf("-ktxVersion: version 1 or version 2\n");
-		printf("-compressionQuality: compression level for KTX2 files in range 0 - 255, 0 -> no compression \n");
 		printf("-targetFormat: specify output texture format (R8G8B8A8_UNORM, R16G16B16A16_SFLOAT, R32G32B32A32_SFLOAT)  \n");
 		printf("-lodBias: level of detail bias applied to filtering (default = 1) \n");
 		printf("-inputIsCubeMap: if set, a cube map in ktx1 format is expected at input path \n");
